56cc79807ff99b2633f53df053fdf564d22e35f7
ackerapple <acker.dawn.apple@gmail.com>
2016-12-13 17:05:43 -0500

    cli fix

--- cli.js
+++ cli.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
+const path = require('path')
 const inPath = relatize(process.argv[1])
 const outPath = relatize(process.argv[2])
+
+function relatize(p){
+  if(p.substring(0, path.sep.length)!=path.sep){
+    p = path.join(process.cwd(), p)
+  }
+  return p
+}
 
 require('./webpacker')(inPath, outPath)
