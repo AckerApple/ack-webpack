b1749a02bb43357878b08fe47ddb382b6a6a2bca
Beautisos <Beautisos@beautisos.local>
2017-02-11 16:04:03 -0500

    test

--- postinstall.js
+++ postinstall.js
@@ -6,15 +6,15 @@ try{
   const pack = require(packPath)
   upgradePack(pack,packPath)
 }catch(e){
-  log('Could not upgrade '+packPath)
-  log(e)
+  log.warn('Could not upgrade '+packPath)
+  log.warn(e)
 }
 
 function upgradePack(pack,packPath){
   pack.scripts = pack.scripts || {}
   if(!pack.scripts['ack-webpack']){
     pack.scripts['ack-webpack'] = 'ack-webpack'
-    log('added package.scripts.ack-webpack to:',packPath)
+    log('added npm run ack-webpack script to:',packPath)
   }
-  fs.writeFileSync(packPath, pack)
+  fs.writeFileSync(packPath, JSON.stringify(pack, null, 2))
 }
